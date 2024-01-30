433e96ab43cd4d0c48ca149213ba6a97b0e5a81f
adrien <adrien.delepine@xemelgo.com>
2024-01-30 13:46:55 -0800

    add swift project

---
+++ frontend/where2eat/where2eat/where2eatApp.swift
@@ -0,0 +1,17 @@
+//
+//  where2eatApp.swift
+//  where2eat
+//
+//  Created by Adrien Delepine on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct where2eatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
